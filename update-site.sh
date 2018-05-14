3a915398ceebeeee58c6e01f8d02a8264da70a34
DA <daniel.appel.winterwell@gmail.com>
2018-05-14 12:16:51 +0100

    made this site into a jerbil site

--- update-site.sh
+++ update-site.sh
@@ -1,7 +1,39 @@
 #!/bin/bash
-printf "\nUpdating the Website...\n"
-printf "\nHeppner is pulling the latest changes from github.com...\n"
-ssh winterwell@heppner.soda.sh 'git --git-dir=/home/winterwell/demo.good-loop.com/.git/ --work-tree=/home/winterwell/demo.good-loop.com gc --prune=now'
-ssh winterwell@heppner.soda.sh 'git --git-dir=/home/winterwell/demo.good-loop.com/.git/ --work-tree=/home/winterwell/demo.good-loop.com pull origin master'
-ssh winterwell@heppner.soda.sh 'git --git-dir=/home/winterwell/demo.good-loop.com/.git/ --work-tree=/home/winterwell/demo.good-loop.com reset --hard FETCH_HEAD'
-printf "\nwebsite updated\n"
+
+PROJECT='Good-Loop Demo Site'
+TARGET='heppner.soda.sh'
+SSHCOMMAND="ssh winterwell@$TARGET"
+TARGETDIR='/home/winterwell/demo.good-loop.com'
+GITSHORTHAND="git --git-dir=$TARGETDIR/.git/ --work-tree=$TARGETDIR"
+
+echo -e "Telling the server to update it's $PROJECT git repo"
+echo -e "> Garbage Collecting..."
+$SSHCOMMAND "$GITSHORTHAND gc --prune=now"
+echo -e "> Pulling Origin..."
+$SSHCOMMAND "$GITSHORTHAND pull origin master"
+echo -e "> Resetting Files to version held on github"
+$SSHCOMMAND "$GITSHORTHAND reset --hard FETCH_HEAD"
+echo -e ""
+
+echo -e "> Optimising Images..."
+$SSHCOMMAND "optipng $TARGETDIR/webroot/img/*.png"
+$SSHCOMMAND "jpegoptim $TARGETDIR/webroot/img/*.jpg"
+$SSHCOMMAND "jpegoptim $TARGETDIR/webroot/img/*.jpeg"
+echo -e ""
+
+echo -e "> Setting permissions on the image files"
+$SSHCOMMAND "chmod 766 $TARGETDIR/webroot/img/*.png"
+$SSHCOMMAND "chmod 766 $TARGETDIR/webroot/img/*.jpg"
+$SSHCOMMAND "chmod 766 $TARGETDIR/webroot/img/*.jpeg"
+echo -e ""
+
+echo -e "> Converting Markdown to HTML..."
+$SSHCOMMAND 'git --git-dir=/home/winterwell/jerbil/.git/ --work-tree=/home/winterwell/jerbil gc --prune=now'
+$SSHCOMMAND 'git --git-dir=/home/winterwell/jerbil/.git/ --work-tree=/home/winterwell/jerbil pull origin master'
+$SSHCOMMAND 'git --git-dir=/home/winterwell/jerbil/.git/ --work-tree=/home/winterwell/jerbil reset --hard FETCH_HEAD'
+rsync -hp ~/winterwell/code/middleware/xstream/xstream.jar winterwell@$TARGET:/home/winterwell/jerbil/lib/
+rsync -hP ~/winterwell/code/middleware/xstream/xmlpull-1.1.3.1.jar winterwell@$TARGET:/home/winterwell/jerbil/lib/
+$SSHCOMMAND "cd /home/winterwell/jerbil/ && java -cp jerbil.jar:lib/* Jerbil $TARGETDIR"
+echo -e ""
+
+echo -e "$PROJECT Website has now been updated"
