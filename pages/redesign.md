575e4a5b4456e4bff071051e6cf6c5cbc0ae98b4
Irina Preda <irinalaurapreda@gmail.com>
2018-10-16 13:21:47 +0100

    added first version of redesign

---
+++ pages/redesign.md
@@ -0,0 +1,108 @@
+<div class="container-fluid light-gray-bg">
+    <!-- Desktop/Laptop Version of the Ad, and text -->
+    <div class="row d-none d-sm-block d-md-block">
+        <div class="col-md-12">
+            <div class="row">
+                <!-- <div class="header-text">OUR ETHICAL ADS SIT IN A RAGE OF STANDARD FORMATS, SUCH AS...</div> -->
+                <div id="demo-wrapper">
+                    <div id="demo-text">OUR ETHICAL ADS SIT IN A RAGE OF STANDARD FORMATS, SUCH AS...</div> 
+                    <div id="demo"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END OF Desktop/Laptop Version of the Ad, and text -->
+    <!-- Mobile version of the Ad and text -->
+    <div class="row d-block d-sm-none d-md-none">
+        <div class="col-md-12">
+            <div class="row">
+                <div id="demo-mobile-wrapper">
+                    <div id="demo-mobile-text">OUR ETHICAL ADS SIT IN A RAGE OF STANDARD FORMATS, SUCH AS...</div> 
+                    <div id="demo-mobile"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END OF Mobile version of the Ad and Text -->
+</div>
+<div class="container-fluid">
+<div class="row appear-after-click">
+    <div class="col-md-6 offset-md-3">
+        <span  class="gl-font-4 justified font-18px d-none d-sm-block d-md-block">
+            SO, WHAT JUST HAPPENED?
+            <br>
+            You opted in to watch an advert, and in exchange, we gave 50% of the ad money to your chosen charity. So you've received a more respectful, more positive advertising experience. And the advertiser has paid for a premium ad view that boosts brand perception, watch time, engagement and more.
+            <br>
+            Nice.
+        </span>
+    </div>
+</div>
+</div>
+<!-- Only Desktops/Laptops See this Row -->
+<div class="container-fluid">
+<div class="row d-none d-sm-block d-md-block">
+    <div class="col-md-12 fit-width">
+            <div class="col-md-8 offset-md-2">
+                <center>
+                    <span class="gl-font-1 gl-page-header-text">
+                        Interested? Get in Touch
+                    </span>
+                </center>
+            </div>
+    </div>
+</div>
+</div>
+<!-- End of the Desktops/Laptops Exclusive Row -->
+<!-- Only Mobile Devices See This Row -->
+<div class="container-fluid">
+<div class="row d-block d-sm-none d-md-none">
+    <div class="col-md-12 fit-width">
+        <div class="col-md-8 offset-md-2">
+            <center>
+                <span class="gl-font-1 gl-page-header-text-mobile-lesser">
+                    Interested? Get in Touch
+                </span>
+            </center>
+        </div>
+    </div>
+</div>
+</div>
+<!-- End of Exclusive Mobile Devices Row -->
+<div class="container-fluid">
+<div class="row">
+    <div class="col-md-12">
+        <!-- Desktop/Laptop Exclusive Form -->
+        <div class="col-md-8 offset-md-2 d-none d-sm-block d-md-block">
+            <div class="col-md-12">
+                <!-- Cheap Vertical Space -->
+                <div style="height:1.5em;">
+                </div>
+                <!-- End of Cheap Vertical Space -->
+                <center>
+                    <form id="contactform" action="https://formspree.io/xowwlvjx" method="POST">
+                        <input type="text" name="_gotcha" style="display: none"> 
+                        <input type="name" class="gl-input-field" name="namefield" placeholder="Name">
+                        <input type="email" class="gl-input-field" name="email" placeholder="Email">
+                        <input type="hidden" name="_next" value="https://www.good-loop.com/success" />
+                        <button class="gl-button-link-desktop" id="sendMessage">Request More Information</button>
+                    </form>
+                </center>
+            </div>
+        </div>
+        <!-- End of Desktop/Laptop Exclusive Form -->
+        <!-- Mobile Devices Exclusive Form -->
+        <div class="col-md-6 offset-md-3 d-block d-sm-none d-md-none">
+            <center>
+                <form id="contactform" action="https://formspree.io/xowwlvjx" method="POST">
+                    <input type="text" name="_gotcha" style="display: none">
+                    <input type="name" class="gl-input-field-mobile" name="namefield" placeholder="Name">
+                    <input type="email" class="gl-input-field-mobile" name="email" placeholder="Email">
+                    <input type="hidden" name="_next" value="https://www.good-loop.com/success" />
+                    <button class="gl-button-link-mobile" id="sendMessage">Request More Information</button>
+                </form>
+            </center>
+        </div>
+        <!-- End of Mobile Devices Exclusive Form -->
+    </div>
+</div>
+</div>
---
+++ webroot/js/redesign.js
@@ -0,0 +1,130 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+
+var Carousel = function (_React$Component) {
+    _inherits(Carousel, _React$Component);
+
+    function Carousel(props) {
+        _classCallCheck(this, Carousel);
+
+        var _this = _possibleConstructorReturn(this, (Carousel.__proto__ || Object.getPrototypeOf(Carousel)).call(this, props));
+
+        _this.state = {
+            items: _this.props.items,
+            active: _this.props.active,
+            show: _this.props.show,
+            direction: ''
+        };
+        _this.rightClick = _this.moveRight.bind(_this);
+        _this.leftClick = _this.moveLeft.bind(_this);
+        return _this;
+    }
+
+    _createClass(Carousel, [{
+        key: 'generateItems',
+        value: function generateItems() {
+            var items = [];
+            var level;
+            var margin = Math.round(this.state.show/2);
+            console.log(margin);
+            for (var i = this.state.active - (margin-1); i < this.state.active + (margin); i++) {
+                var index = i;
+                if (i < 0) {
+                    index = this.state.items.length + i;
+                } else if (i >= this.state.items.length) {
+                    index = i % this.state.items.length;
+                }
+                level = this.state.active - i;
+                items.push(React.createElement(Item, { key: index, id: this.state.items[index], level: level }));
+            }
+            return items;
+        }
+    }, {
+        key: 'moveLeft',
+        value: function moveLeft() {
+            var newActive = this.state.active;
+            newActive--;
+            this.setState({
+                active: newActive < 0 ? this.state.items.length - 1 : newActive,
+                direction: 'left'
+            });
+        }
+    }, {
+        key: 'moveRight',
+        value: function moveRight() {
+            var newActive = this.state.active;
+            this.setState({
+                active: (newActive + 1) % this.state.items.length,
+                direction: 'right'
+            });
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                { id: 'carousel', className: 'noselect' },
+                React.createElement(
+                    'div',
+                    { className: 'arrow arrow-left', onClick: this.leftClick },
+                    React.createElement('i', { className: 'fi-arrow-left' })
+                ),
+                React.createElement(
+                    ReactCSSTransitionGroup,
+                    {
+                        transitionName: this.state.direction },
+                    this.generateItems()
+                ),
+                React.createElement(
+                    'div',
+                    { className: 'arrow arrow-right', onClick: this.rightClick },
+                    React.createElement('i', { className: 'fi-arrow-right' })
+                )
+            );
+        }
+    }]);
+
+    return Carousel;
+}(React.Component);
+
+var Item = function (_React$Component2) {
+    _inherits(Item, _React$Component2);
+
+    function Item(props) {
+        _classCallCheck(this, Item);
+
+        var _this2 = _possibleConstructorReturn(this, (Item.__proto__ || Object.getPrototypeOf(Item)).call(this, props));
+
+        _this2.state = {
+            level: _this2.props.level
+        };
+        return _this2;
+    }
+
+    _createClass(Item, [{
+        key: 'render',
+        value: function render() {
+            var className = 'outer item level' + this.props.level;
+            return React.createElement(
+                'div',
+                { className: className },
+                React.createElement('iframe', { className: "inner image img" + this.props.id, src: 'https://codepen.io/chrisgannon/full/NGGKWo/', frameborder: '0', allowfullscreen: true })
+            );
+        }
+    }]);
+
+    return Item;
+}(React.Component);
+
+var items = [1, 2, 3, 4];
+ReactDOM.render(React.createElement(Carousel, { items: items, active: 0, show: 3 }), document.getElementById('demo'));
+ReactDOM.render(React.createElement(Carousel, { items: items, active: 0, show: 1 }), document.getElementById('demo-mobile'));
