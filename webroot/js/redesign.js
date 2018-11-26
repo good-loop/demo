bcfa00087373eacf55a85bda8b2110b94aedaf8a
Irina Preda <irinalaurapreda@gmail.com>
2018-11-26 17:17:16 +0000

    committing forgotten items from last week

--- webroot/js/redesign.js
+++ webroot/js/redesign.js
@@ -5,6 +5,7 @@ var links = ['/mpu2.html',
             '/mpu.html'];
 
 function clickedItem(selectedIndex, mobile=false) {
-    ReactDOM.render(React.createElement('span', {className: ''}, items[selectedIndex]), document.getElementById(mobile ? 'demo-mobile-adtype' : 'demo-adtype'));
-    ReactDOM.render(React.createElement('iframe', {className: 'image', src: links[selectedIndex], frameBorder: '0'}, items[0]), document.getElementById(mobile ? 'wrapper-iframe' : 'outer'));
+    ReactDOM.render(React.createElement('span', {className: items[selectedIndex]}, items[selectedIndex]), document.getElementById(mobile ? 'demo-mobile-adtype' : 'demo-adtype'));
+    ReactDOM.render(React.createElement('iframe', {className: 'image', src: links[selectedIndex], frameBorder: '0' }, items[0]), document.getElementById(mobile ? 'wrapper-iframe' : 'outer'));
+
 }
