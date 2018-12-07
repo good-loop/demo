d138034071a6351ba8ad43d0cf2d51cc9ab57542
Irina Preda <irinalaurapreda@gmail.com>
2018-12-07 18:00:25 +0000

    fixed URL query param passing

--- webroot/js/redesign.js
+++ webroot/js/redesign.js
@@ -4,11 +4,19 @@ var links = ['/mpu2.html',
             '/in-read.html', 
             '/mpu.html'];
 
+// default ad shown on demo page
+// add iframe to id, making sure to pass URL params 
+var iframeDiv = document.getElementsByClassName('image');
+// make sure to add it to both mobile and desktop
+iframeDiv[0].src = links[0] + window.location.search;
+iframeDiv[1].src = links[0] + window.location.search;
+
+// click to go to different types of ads on demo page
 function clickedItem(selectedIndex, mobile) {
-//    ReactDOM.render(React.createElement('span', {className: ''}, items[selectedIndex]), document.getElementById(mobile ? 'demo-mobile-adtype' : 'demo-adtype'));
     var adNameDiv = document.getElementById(mobile ? 'demo-mobile-adtype' : 'demo-adtype');
     adNameDiv.innerHTML = '<span>' + items[selectedIndex] + '</span>';
-//    ReactDOM.render(React.createElement('iframe', {className: 'image', src: links[selectedIndex], frameBorder: '0'}, items[0]), document.getElementById(mobile ? 'wrapper-iframe' : 'outer'));
     var iframeDiv = document.getElementById(mobile ? 'wrapper-iframe' : 'outer');
-    iframeDiv.innerHTML = '<iframe class="image" frameborder=0 src=' + links[selectedIndex] + '></iframe>';
+    // add iframe to id, making sure to pass URL params
+    iframeDiv.innerHTML = '<iframe class="image" frameborder=0 src="' + links[selectedIndex] + window.location.search + '"></iframe>';
 }
+
