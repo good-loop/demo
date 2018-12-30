5dd19a4f744d4ddff58401044784f396464f0f77
Daniel Winterstein <daniel@sodash.com>
2018-12-30 18:19:23 +0000

    the horror

---
+++ convert.less.sh
@@ -0,0 +1,52 @@
+# !/bin/env bash
+
+WATCH=$1
+GOTINOTIFYTOOLS=`which inotifywait`
+DIR=`pwd`;
+PROJECT=`basename $DIR`
+echo "PROJECT: $PROJECT"
+#PROJECTPATH=/home/$USER/winterwell/code/$PROJECT
+#PROJECTPATH=/home/$USER/winterwell/$PROJECT
+PROJECTPATH=$DIR
+WEB=$PROJECTPATH/webroot # non-standard!
+OUTDIR=$WEB/style
+
+# the TOPLESS files are the top level files referenced in index.html
+TOPLESS[0]=$PROJECTPATH/src/style/main.less;
+TOPLESS[1]=$PROJECTPATH/src/style/print.less;
+
+# run through files
+for file in "${TOPLESS[@]}"; do
+		if [ -e "$file" ]; then
+			echo -e "converting $file"
+			F=`basename $file`
+			echo lessc "$file" "$OUTDIR/${F%.less}.css"
+			lessc "$file" "$OUTDIR/${F%.less}.css"
+		else
+			echo "less file not found: $file"				
+		fi
+done
+
+# watch?
+if [[ $WATCH == 'watch' ]]; then
+	if [ "$GOTINOTIFYTOOLS" = "" ]; then
+    	echo "In order to watch and continuously convert less files, you will first need to install inotify-tools on this system"
+    	echo ""
+    	echo "run sudo apt-get install inotify-tools in order to install"
+    	exit 0
+	else
+	while true
+	do
+		inotifywait -r -e modify,attrib,close_write,move,create,delete $WEB/style && \
+		for file in "${TOPLESS[@]}"; do
+			if [ -e "$file" ]; then
+				echo -e "converting $file"
+				F=`basename $file`
+				lessc "$file" "$OUTDIR/${F%.less}.css"
+			else
+				echo "less file not found: $file"
+			fi
+		done
+	done
+	fi
+fi
--- webroot/js/demo.js
+++ webroot/js/demo.js
@@ -1,12 +1,3 @@
-$('#goodloopad-mobile').click(function() {
-    $('#appear-after-click').css({
-        'display': 'block'
-    });
-    $('#goodloopad-mobile').css({
-        'display': 'none'
-    });
-});
-
 
 $('.goodloopad').click(function() {
     $('.appear-after-click').css({
@@ -33,3 +24,26 @@ $('body').on('gl:adblock', function() {
 	// TODO a message to the user.
 	console.warn("Adblocker detected - expect some things to break.");
 });
+
+var items = ['Double-MPU', 'Pre-roll', 'In-read', 'Click-to-expand'];
+var links = ['/mpu2.html',
+            '/pre-roll.html', 
+            '/in-read.html', 
+            '/mpu.html'];
+
+// default ad shown on demo page
+// add iframe to id, making sure to pass URL params 
+var iframeDiv = document.getElementsByClassName('image');
+// make sure to add it to both mobile and desktop
+iframeDiv[0].src = links[0] + window.location.search;
+iframeDiv[1].src = links[0] + window.location.search;
+
+// click to go to different types of ads on demo page
+function clickedItem(selectedIndex, mobile) {
+    var adNameDiv = document.getElementById('demo-adtype');
+    adNameDiv.innerHTML = '<span>' + items[selectedIndex] + '</span>';
+    var iframeDiv = document.getElementById(mobile ? 'wrapper-iframe' : 'outer');
+    // add iframe to id, making sure to pass URL params
+    iframeDiv.innerHTML = '<iframe class="image" frameborder=0 src="' + links[selectedIndex] + window.location.search + '"></iframe>';
+}
+
