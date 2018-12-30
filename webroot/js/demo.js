33b8a646953eaa7fd6c457ebfbacdae6b52624d1
Daniel Winterstein <daniel@sodash.com>
2018-12-30 20:32:31 +0000

    cleaning-up

--- webroot/js/demo.js
+++ webroot/js/demo.js
@@ -21,7 +21,6 @@ $('body').on('gl:adblock', function() {
     $('.adblock-warning').css({
         'display': 'block'
     });
-	// TODO a message to the user.
 	console.warn("Adblocker detected - expect some things to break.");
 });
 
@@ -31,18 +30,19 @@ var links = ['/mpu2.html',
             '/in-read.html', 
             '/mpu.html'];
 
-// default ad shown on demo page
+// default ad shown on demo page (passing on any demo page url params)
 // add iframe to id, making sure to pass URL params 
-var iframeDiv = document.getElementsByClassName('image');
+var iframeDivs = document.getElementsByClassName('image');
 // make sure to add it to both mobile and desktop
-iframeDiv[0].src = links[0] + window.location.search;
-iframeDiv[1].src = links[0] + window.location.search;
+for(var i=0; i<iframeDivs.length; i++) {
+	iframeDivs[i].src = links[0] + window.location.search;
+}
 
 // click to go to different types of ads on demo page
-function clickedItem(selectedIndex, mobile) {
+function clickedItem(selectedIndex) {
     var adNameDiv = document.getElementById('demo-adtype');
     adNameDiv.innerHTML = '<span>' + items[selectedIndex] + '</span>';
-    var iframeDiv = document.getElementById(mobile ? 'wrapper-iframe' : 'outer');
+    var iframeDiv = document.getElementById('outer');
     // add iframe to id, making sure to pass URL params
     iframeDiv.innerHTML = '<iframe class="image" frameborder=0 src="' + links[selectedIndex] + window.location.search + '"></iframe>';
 }
