48f59f59095a692fbb4b4010a428b10917ed7204
aundiks@gmail.com <andris@good-loop.com>
2020-01-13 13:33:38 +0000

    TestSite: Testing apn tags.

--- src/components/test/AppNexusAdPage.jsx
+++ src/components/test/AppNexusAdPage.jsx
@@ -2,14 +2,33 @@ import { h, Fragment } from 'preact';
 import { Container, Row } from 'reactstrap';
 
 const AppNexusAdPage = () => {
-  
+
     return (
         <Container>
-            <Row>
+            <div className="d-flex flex-column">
                 Below the placement
-            </Row>
-            <Row>
-            </Row>
+                <h1>Using AppNexus Publisher Ad Server</h1>
+                <h5>Div-1</h5>
+                <div id="div-1">
+                    <script type="text/javascript">{
+                        apntag.anq.push(function() {
+                        apntag.showTag('div-1');
+                    })}
+
+                    </script>
+                </div>
+
+                <br />
+
+                    <h5>Div-2</h5>
+                    <div id="div-2">
+                        <script type="text/javascript"> {
+                            apntag.anq.push(function() {
+                            apntag.showTag('div-2');
+                            })}
+                        </script>
+                    </div>
+            </div>
         </Container>
     )
 };
