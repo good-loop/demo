16f77933d2851ed038b4cdc227f5ea0e255bc0d6
aundiks@gmail.com <andris@good-loop.com>
2020-01-27 15:22:36 +0000

    PuppeteerTests: Improved runner, update to work with wiki's instructions.

---
+++ runtest.js
@@ -0,0 +1,33 @@
+'use strict';
+const shell = require('shelljs');
+const yargv = require('yargs').argv;
+
+let config = {
+  site: 'test',
+  unsafe: false,
+  flag: false,
+  head: true
+};
+let argv = process.argv.slice(0, 2);
+
+let testPath = '';
+
+Object.entries(yargv).forEach(([key, value]) => {
+  if (key === 'test') { testPath = value; }
+
+  if (Object.keys(config).includes(key)) {
+    if (typeof config[key] === "boolean") {
+      const bool = config[key];
+      config[key] = !bool;
+    } else config[key] = value;
+  }
+});
+
+// Store configuration on env
+process.env.__CONFIGURATION = JSON.stringify(config);
+
+// Setting real ARGV
+process.argv = argv;
+
+// Execute Jest. Specific target optional.
+shell.exec(`npm run test${' ' + testPath}`);
