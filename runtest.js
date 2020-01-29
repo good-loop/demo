be4dc2df2fff6b992345c6bc5eed5b5b210043a4
aundiks@gmail.com <andris@good-loop.com>
2020-01-29 15:28:56 +0000

    Puppeteer: Detailed comments for reference on demo.test.js / Demo: Put back code to insert all vertIds into the url

--- runtest.js
+++ runtest.js
@@ -3,24 +3,26 @@ const shell = require('shelljs');
 const yargv = require('yargs').argv;
 
 let config = {
-  site: 'test',
-  unsafe: false,
-  flag: false,
-  head: true
+	site: 'test',
+	unsafe: false,
+	flag: false,
+	head: true
 };
 let argv = process.argv.slice(0, 2);
 
 let testPath = '';
+let runInBand = '';
 
 Object.entries(yargv).forEach(([key, value]) => {
-  if (key === 'test') { testPath = value; }
+	if (key === 'test') { testPath = value; }
+	if (key === 'runInBand') { runInBand = '--runInBand'; }
 
-  if (Object.keys(config).includes(key)) {
-    if (typeof config[key] === "boolean") {
-      const bool = config[key];
-      config[key] = !bool;
-    } else config[key] = value;
-  }
+	if (Object.keys(config).includes(key)) {
+		if (typeof config[key] === "boolean") {
+			const bool = config[key];
+			config[key] = !bool;
+		} else config[key] = value;
+	}
 });
 
 // Store configuration on env
@@ -30,4 +32,4 @@ process.env.__CONFIGURATION = JSON.stringify(config);
 process.argv = argv;
 
 // Execute Jest. Specific target optional.
-shell.exec(`npm run test${' ' + testPath}`);
+shell.exec(`npm run test${' ' + testPath + runInBand}`);
