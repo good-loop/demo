50930a297ecdaad386a4a6903c7014c3f754c7c3
Roscoe <roscoe@winterwell.com>
2020-01-30 11:52:15 +0000

    Fix issue where production demo site was defaulting to testas.good-loop.com/unit.js

--- src/utils.js
+++ src/utils.js
@@ -14,9 +14,9 @@ export const getNonce = (props) => {
 	return props.size + glParamString + props.extraNonce;
 };
 
-let prefix = 'test';
-if (window.location.hostname.match(/^local/)) { prefix = 'local'; } // Running on localtest.good-loop.com --> talk to localas
-else if (window.location.hostname.match(/^prod/)) { prefix = ''; } // Running on prodtest.good-loop.com --> talk to as
+let prefix = ''; // Default to production adserver (ie running on demo or prodtest.good-loop.com)
+if (window.location.hostname.match(/^local/)) { prefix = 'local'; } // Running on localtest or localdemo.good-loop.com --> talk to localas
+else if (window.location.hostname.match(/(^test)/)) { prefix = 'test'; } // Running on test or testdemo.good-loop.com --> talk to testas
 const glBaseUrl = `${window.location.protocol}//${prefix}as.good-loop.com/`
 const glProdBaseUrl = `https://as.good-loop.com/`;
 
