d7a67d9d6aa1be99800d54a90288388b7e3dfde2
Roscoe <roscoe@good-loop.com>
2022-04-07 17:57:33 +0100

    Demo page: Fix ugliness when advert locked to landscape format is viewed in portrait frame

--- src/components/GoodLoopAd.jsx
+++ src/components/GoodLoopAd.jsx
@@ -12,6 +12,36 @@ const loadingStyle = {
 	backgroundColor: 'white'
 };
 
+const unitTypeToSize = {
+	landscape: 'landscape',
+	portrait: 'portrait',
+	trees: 'landscape',
+};
+
+const playerVariantToSize = {
+	'landscape-a': 'landscape',
+	'landscape-b': 'landscape',
+	'landscape-tall': 'landscape',
+	'trees': 'landscape',
+	'trees-xmas': 'landscape',
+};
+
+/**
+ * Approximately replicate the adunit's unit-type selection behaviour
+ * so we can size the frame correctly in cases like the Levi's ad where
+ * it will try to render as landscape regardless of container
+ */
+const sizeFromUnit = (unitObj) => {
+	// URL param takes #1 priority...
+	const unitTypeParam = new URLSearchParams(window.location.search).get('gl.unitType');
+	if (unitTypeParam && unitTypeToSize[unitTypeParam]) return unitTypeToSize[unitTypeParam];
+	// ...and then vert.advanced.playerVariant
+	const playerVariant = unitObj.vert.advanced.playerVariant;
+	if (playerVariant && playerVariantToSize[playerVariant]) return playerVariantToSize[playerVariant];
+	// Looks like the unit has no conclusive opinions about the size/shape of container it should be in.
+	return null;
+};
+
 /**
  * How is this different from the React GoodLoopAd.jsx used in the portal?
  * This takes advantage of the fact that - unlike in React - Preact allows you to insert a <script>
@@ -29,14 +59,20 @@ const GoodLoopAd = ({size, vertId, bare, extraNonce, refPolicy = 'no-referrer-wh
 	// Load the ad
 	const [unitJson, setUnitJson] = useState(null); // Preloaded unit.json
 	const [unitBranch, setUnitBranch] = useState(false); // Extracted from unit.json: vert.legacyUnitBranch
+	const [sizeOverride, setSizeOverride] = useState(); // Is the advert set up to force a different size from the container?
 
 	// Fetch the advert from *as.good-loop.com so we can check if it has a legacy branch
 	useEffect(() => {
 		// respect new dataServer (ie "get unit.json from TEST") param, fall back to old forceServerType param
 		const forceServerType = params.dataServer || params.forceServerType;
 		getAdvertFromAS({id: vertId, params: {...params, forceServerType} }).then(unitObj => {
-			setUnitBranch(unitObj.vert.legacyUnitBranch || '');
 			setUnitJson(JSON.stringify(unitObj));
+			setUnitBranch(unitObj.vert.legacyUnitBranch || '');
+			// Do we need to use a weird sizer like landscape-in-portrait?
+			const sfu = sizeFromUnit(unitObj);
+			if (sfu && sfu !== size) {
+				setSizeOverride(sfu);
+			}
 		});
 	}, [vertId]);
 
@@ -54,7 +90,7 @@ const GoodLoopAd = ({size, vertId, bare, extraNonce, refPolicy = 'no-referrer-wh
 		const unitUrl = getAdUrl({file: 'unit.js', ...params, forceServerType, unitBranch});
 
 		adElements = <>
-			<div className="goodloopad" data-format={size} data-mobile-format={size} key={nonce + '-container'} />
+			<div className="goodloopad" data-format={sizeOverride || size} data-mobile-format={sizeOverride || size} key={nonce + '-container'} />
 			<script src={unitUrl} key={nonce + '-script'} referrerPolicy={refPolicy} />
 			<script id="preloaded-unit-json" type="application/json">{unitJson}</script>
 		</>;
@@ -63,9 +99,11 @@ const GoodLoopAd = ({size, vertId, bare, extraNonce, refPolicy = 'no-referrer-wh
 	// Aspectifier isn't always wanted - eg in fullscreen mode where making the
 	// container 100% width and forcing it to 16:9 aspect will cause overflow
 	if (bare) return adElements;
+
+	const sizerClasses = ['ad-sizer', size, sizeOverride && `${sizeOverride}-in-${size}`].filter(a => !!a).join(' ');
 	
 	return (
-		<div className={`ad-sizer ${size}`} key={nonce + '-direct'}>
+		<div className={sizerClasses} key={nonce + '-direct'}>
 			<div className="aspectifier" />
 			{adElements}
 		</div>
